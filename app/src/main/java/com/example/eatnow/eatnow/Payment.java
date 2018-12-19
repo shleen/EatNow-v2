7a4e99396c0cf671a23d9fab94bc8c1381b4d1f7
Jianwen Rayner Tan <s10177641@connect.np.edu.sg>
2018-12-19 16:07:53 +0800

    Payment

---
+++ app/src/main/java/com/example/eatnow/eatnow/Payment.java
@@ -0,0 +1,19 @@
+package com.example.eatnow.eatnow;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Payment extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+
+
+    }
+}
