a058fe108dc9dca671386c214e83dfddaa9c0fd0
shleen <sim.sheline@gmail.com>
2018-12-23 15:51:41 +0800

    New launch page

--- app/src/main/java/com/example/eatnow/eatnow/MainActivity.java
+++ app/src/main/java/com/example/eatnow/eatnow/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,17 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide app bar
+        getSupportActionBar().hide();
+
+        TextView lkSignIn = (TextView) findViewById(R.id.lkSignIn);
+        lkSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
-    public void openRegister(View v)
+    public void openSignUp(View v)
     {
         Intent i = new Intent(this, RegisterActivity.class);
-        startActivity(i);
-    }
-
-    public void openLogin(View v)
-    {
-        Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
     }
 }
