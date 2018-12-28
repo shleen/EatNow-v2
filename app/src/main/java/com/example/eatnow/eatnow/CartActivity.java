4beebb8c95c19e3b45e9219a93d9553ab0ea8306
shleen <sim.sheline@gmail.com>
2018-12-28 14:50:51 +0800

    Remove unnecessary imports

--- app/src/main/java/com/example/eatnow/eatnow/CartActivity.java
+++ app/src/main/java/com/example/eatnow/eatnow/CartActivity.java
@@ -1,10 +1,8 @@
 package com.example.eatnow.eatnow;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
--- app/src/main/java/com/example/eatnow/eatnow/MenuActivity.java
+++ app/src/main/java/com/example/eatnow/eatnow/MenuActivity.java
@@ -4,17 +4,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
