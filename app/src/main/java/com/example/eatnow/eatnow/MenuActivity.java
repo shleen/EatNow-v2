67d1d51fbd05e042445e7bfdfeaa369d7a4709bc
shleen <sim.sheline@gmail.com>
2018-12-30 22:04:21 +0800

    Remove unused import

--- app/src/main/java/com/example/eatnow/eatnow/MenuActivity.java
+++ app/src/main/java/com/example/eatnow/eatnow/MenuActivity.java
@@ -7,7 +7,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
