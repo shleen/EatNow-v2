6bb8a19446a8193a65dd6d2d26e257bfb00dc872
shleen <sim.sheline@gmail.com>
2019-01-08 21:47:40 +0800

    Add RobotActivity

---
+++ app/src/main/java/com/example/eatnow/eatnow/RobotActivity.java
@@ -0,0 +1,13 @@
+package com.example.eatnow.eatnow;
+
+import android.os.Bundle;
+
+public class RobotActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_robot);
+        super.onCreate(savedInstanceState);
+
+    }
+}
