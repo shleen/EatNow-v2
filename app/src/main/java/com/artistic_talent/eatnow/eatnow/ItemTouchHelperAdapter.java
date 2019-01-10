10f5bac1d6ec906a3ed547e1a8d71ff1b7c961a1
shleen <sim.sheline@gmail.com>
2019-01-10 23:52:25 +0800

    Update ItemTouchHelperAdapter interface

--- app/src/main/java/com/artistic_talent/eatnow/eatnow/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/artistic_talent/eatnow/eatnow/ItemTouchHelperAdapter.java
@@ -1,8 +1,5 @@
 package com.artistic_talent.eatnow.eatnow;
 
 public interface ItemTouchHelperAdapter {
-
-    // boolean onItemMove(int fromPosition, int toPosition);
-
     void onItemDismiss(int position);
 }
