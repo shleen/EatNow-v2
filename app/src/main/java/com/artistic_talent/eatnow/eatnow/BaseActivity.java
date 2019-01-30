4e2d6af06afbb6f83feab70610baa84c4eda2696
Jianwen Rayner Tan <s10177641@connect.np.edu.sg>
2019-01-30 20:37:38 +0800

    Wesley Teo dah best

--- app/src/main/java/com/artistic_talent/eatnow/eatnow/BaseActivity.java
+++ app/src/main/java/com/artistic_talent/eatnow/eatnow/BaseActivity.java
@@ -23,6 +23,7 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         navigationView = findViewById(R.id.nav_view);
@@ -39,7 +40,9 @@ public class BaseActivity extends AppCompatActivity {
                         } else if (id == R.id.nav_cart) {
                             i = new Intent(getApplicationContext(), CartActivity.class);
                         } else if (id == R.id.nav_orders) {
+
                             i = new Intent(getApplicationContext(),OrderActivity.class);
+
 
                         } else if (id == R.id.nav_qr) {
                             i = new Intent(getApplicationContext(), QRCodeActivity.class);
@@ -49,6 +52,7 @@ public class BaseActivity extends AppCompatActivity {
                         }
 
                         startActivity(i);
+
 
                         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
                         drawer.closeDrawer(GravityCompat.START);
--- app/src/main/java/com/artistic_talent/eatnow/eatnow/OrderActivity.java
+++ app/src/main/java/com/artistic_talent/eatnow/eatnow/OrderActivity.java
@@ -2,12 +2,28 @@ package com.artistic_talent.eatnow.eatnow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class OrderActivity extends AppCompatActivity {
+    TextView txt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        //Display shit
+        txt1 = findViewById(R.id.textView);
+        //Get user email
+
+        String email = MyApplication.someVariable;
+        txt1.setText(email);
+        //Search firebase for this shit
+
+        //Display
+
+
+
     }
 }
